4d64287036aafe14839f757bed190262d1c4235f
sureshraksha7 <sureshraksha7@gmail.com>
2026-02-20 12:31:13 +0530

    Add placements_collection to models

--- backend/models.py
+++ backend/models.py
@@ -6,3 +6,4 @@ categories_collection = db["categories"]
 backlinks_collection = db["backlinks"]
 project_media_collection = db["project_media"]
 tools_collection = db["tools"]   # NEW
+placements_collection = db["placements"]
